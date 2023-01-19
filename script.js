31b6363826d30344274dcf70cc2074ac84ee8a59
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-01-19 22:49:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+  const mapping = {
+  '2': 'abc',
+  '3': 'def',
+  '4': 'ghi',
+  '5': 'jkl',
+  '6': 'mno',
+  '7': 'pqrs',
+  '8': 'tuv',
+  '9': 'wxyz',
+  '0': '0',
+  '1': '1'
+};
+
+function letterCombinations(digits) {
+    if(!digits) return []
+    let result = [''];
+    for (let i = 0; i < digits.length; i++) {
+        let currentDigit = digits[i];
+        let temp = []
+        for (let j = 0; j < result.length; j++) {
+            for (let k = 0; k < mapping[currentDigit].length; k++) {
+                temp.push(result[j] + mapping[currentDigit][k]);
+            }
+        }
+        result = temp;
+    }
+    return result.sort();
 }
+
+// }
 
 module.exports = letterCombinations;
